3f5f71056ad50d16b8b5b8045d9ab8cb52ab3d84
navonn3 <70155107+navonn3@users.noreply.github.com>
2025-10-14 01:56:18 +0300

    Update Players.jsx

--- src/pages/Players.jsx
+++ src/pages/Players.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { base44 } from "@/api/base44Client";
+import { supabase } from "@/api/supabaseClient";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
